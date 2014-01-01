f5f4f4788b78d7ca6cfca5288c34ecb0ae107c88
lakshmipraba <lakshmipraba@users.noreply.github.com>
2014-01-01 12:41:07 +0530

    Create ex3.py

---
+++ ex3.py
@@ -0,0 +1,8 @@
+print"i'll count my chickens:"
+print"hens:",25+30%6
+print"roosters:",100-25*3%4
+print"now i'll cout the eggs:"
+print 3+2+1-5+4%2-1/4+6
+print"is it true that 3+2<5-7?"
+print 3+2<5-7
+print"5>-2 is it greater?",5>-2
