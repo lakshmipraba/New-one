f7962185bab5dbd1e8943d791c67f35fe5c02f65
lakshmipraba <lakshmipraba@users.noreply.github.com>
2014-01-01 12:54:09 +0530

    Create ex10.py

---
+++ ex10.py
@@ -0,0 +1,13 @@
+tabby_cat="\ti'm tabbled in."
+persian_cat="i'm split\non a line."
+backslash_cat="i'm\\a\\cat."
+fat_cat="""
+i'll do a list:
+\t* cat food
+\t* fishies
+\t* caption\n\t* grass
+"""
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
