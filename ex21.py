53fc0c227de909f5660fa2bcf17cd153b9b96cca
lakshmipraba <lakshmipraba@users.noreply.github.com>
2014-01-01 13:07:28 +0530

    Create ex21.py

---
+++ ex21.py
@@ -0,0 +1,19 @@
+def add(a,b):
+ print"adding %d+%d"%(a,b)
+ return a+b
+def subtract(a,b):
+ print"subtracting %d - %d"%(a,b)
+ return a-b
+def multiply(a,b):
+ print"multiplying %d*%d"%(a,b)
+ return a*b
+def divide(a,b):
+ print"dividing %d/%d"%(a,b)
+ return a/b
+print"math function"
+age=add(30,20)
+height=subtract(50,44)
+weight=multiply(20,3)
+iq=divide(100,2.5)
+print"age: %d\n height:%d\n weight:%d\n iq:%d"%(age,height,weight,iq)
+ 
