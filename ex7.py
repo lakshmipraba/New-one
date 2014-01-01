1a1253b98e0c00d302baaea6e5ec2b6a25daf3da
lakshmipraba <lakshmipraba@users.noreply.github.com>
2014-01-01 12:48:16 +0530

    Create ex7.py

---
+++ ex7.py
@@ -0,0 +1,17 @@
+print"i had a little lamp."
+print"it's fleece was white like %d" % 100
+print"."*10
+end1="l"
+end2="a"
+end3="k"
+end4="s"
+end5="h"
+end6="m"
+end7="i"
+end8="p"
+end9="r"
+end10="a"
+end11="b"
+end12="a"
+print end1+end2+end3+end4+end5+end6+end7,
+print end8+end9+end10+end11+end12
