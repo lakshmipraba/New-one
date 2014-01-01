eca65afc9f44c30cd9260040d546c83cd050a303
lakshmipraba <lakshmipraba@users.noreply.github.com>
2014-01-01 13:11:45 +0530

    Create ex23.py

---
+++ ex23.py
@@ -0,0 +1,18 @@
+boys= 42
+girls=40
+dogs=30
+if boys<girls:
+ print "Too many girls!"
+if boys>girls:
+ print "Not many girls!"
+if boys<dogs:
+ print "many dogs!"
+if boys>dogs:
+ print "many boys!"
+dogs+= 5
+if boys>= dogs:
+ print "boys are greater than or equal to dogs."
+if boys<= dogs:
+ print "boys are less than or equal to dogs."
+if boys == dogs:
+ print "boys are dogs."
