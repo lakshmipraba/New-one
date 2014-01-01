18b6dc1399813bf759630196683966b2919abec2
lakshmipraba <lakshmipraba@users.noreply.github.com>
2014-01-01 12:45:35 +0530

    Create ex5.py

---
+++ ex5.py
@@ -0,0 +1,12 @@
+my_name='ram'
+my_age=21
+my_height=5.7
+my_weight=60
+my_eyes='black'
+my_teeth='white'
+my_hair='black'
+print"let's talk abt %s."% my_name
+print"she's %d feet tall."% my_height
+print"she's %d pounds heavy."% my_weight
+print" %s got %s eyes and %s hair."%(my_name,my_eyes,my_hair)
+print" %s got beautifull %s hair with %d weight."%(my_name,my_hair,my_weight)
