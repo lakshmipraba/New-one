68f874e25ac8f4799752cba1250766bf09d29c0f
lakshmipraba <lakshmipraba@users.noreply.github.com>
2014-01-01 13:17:25 +0530

    Create ex27.py

---
+++ ex27.py
@@ -0,0 +1,11 @@
+i=0
+num=[]
+while i<6:
+ print"at the top i is %d"%i
+ num.append(i)
+ i=i+1
+ print"numbers now",num
+ print"at the bottom i is %d"%i
+print"the numbers:"
+for numbers in num:
+ print numbers
