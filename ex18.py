c1de13fc182927a3bcc544f3dd29a8887d8d8815
lakshmipraba <lakshmipraba@users.noreply.github.com>
2014-01-01 13:03:35 +0530

    Create ex18.py

---
+++ ex18.py
@@ -0,0 +1,11 @@
+def first(*args):
+ arg1,arg2=args
+ print"arg1:%r,arg2:%r"%(arg1,arg2)
+def second(arg1,arg2):
+ print"arg1:%r,arg2:%r"%(arg1,arg2)
+def final():
+ print"nothing"
+first("lakshmi","praba")
+second("praba","lakshmi")
+final() 
+ 
