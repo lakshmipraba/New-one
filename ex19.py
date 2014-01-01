22ba0b4623e5b43756becac8281ee6bc8866d539
lakshmipraba <lakshmipraba@users.noreply.github.com>
2014-01-01 13:04:44 +0530

    Create ex19.py

---
+++ ex19.py
@@ -0,0 +1,22 @@
+def newyear(sweets,gifts):
+ print" u have %d sweets"%sweets
+ print"u have %d gifts"%gifts
+ print"man feel happy"
+ print"thank you"
+print"give value directly:"
+newyear(100,10)
+print"use variables frm script"
+sweets=110
+gifts=7
+newyear(sweets,gifts)
+print"math inside"
+newyear(sweets+100,gifts+10)
+print sweets
+print gifts
+newyear(210,21)
+gifts=21
+print sweets
+print gifts
+
+
+
