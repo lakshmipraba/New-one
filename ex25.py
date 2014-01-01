7b0b532f13b9fde5281edad8b78099b6f25c1bf0
lakshmipraba <lakshmipraba@users.noreply.github.com>
2014-01-01 13:14:15 +0530

    Create ex25.py

---
+++ ex25.py
@@ -0,0 +1,16 @@
+print"u enter a dark room with 2 doors,do u go through door #1 or #2?"
+door=raw_input(">")
+if door == "1":
+ print"there is a bear eat cake.wat do u do?"
+ print"1.take the cake"
+ print"2.smilling"
+ bear=raw_input(">")
+ if bear == "1":
+   print"the bear kill u"
+ elif bear == "2":
+    print"also smile"
+ else:
+   print"%s scream at u"% bear
+else: 
+ print"this is a correct way"
+    
