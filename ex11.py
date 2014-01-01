228ec8c420dcf0bb785bfd42a13164a659b3f1fa
lakshmipraba <lakshmipraba@users.noreply.github.com>
2014-01-01 12:55:35 +0530

    Create ex11.py

---
+++ ex11.py
@@ -0,0 +1,7 @@
+print"how old are you?",
+age=raw_input()
+print"how tall are you?",
+height=raw_input()
+print"what are you doing?",
+work=raw_input()
+print"so,u are %r old,%r tall and %s work."%(age,height,work)
