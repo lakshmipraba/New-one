d4f096a28abe28c36c31b8aa376fb2ba1f00c5a0
lakshmipraba <lakshmipraba@users.noreply.github.com>
2014-01-01 12:52:27 +0530

    Create ex9.py

---
+++ ex9.py
@@ -0,0 +1,13 @@
+days="mon tue wed thu fri sat sun"
+months="jan\nfeb\nmar\napr\nmay\njun"
+print "here are the days:",days
+print "here are the months:",months
+print"""going
+something
+somethig,something
+dsfghjkk
+dsfdvgbfghnkj
+fdgjmk,lfxcgvhj
+fcghjkn
+fghjk
+dfghjk"""
